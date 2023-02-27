c2d5633d35375504ca87c956c276f75be20db26b
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-02-27 14:51:33 +0200

    updated backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,46 +1,39 @@
-'''
-Start an application with a command python backend.py
-The application can be tested with a browser 
-localhost:5000/api?message=joo
-localhost:5000/api?message=ei
-localhost:5000/api?message=vaihda
-localhost:5000/api?message=something else
-Route '/confused' will be deleted from the final version
-'''
 from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
+#the api takes you direclty to the tensorflow or open ai solution if you wish,
+#or decides itself based on the length of the message
+#http://localhost:5000/api?message=<random marks> takes you to one, based on the amount of marks
+#we added two endpoints for openai and tensorflow as well, to follow the diagram, but they dont account the
+#message yet
+#http://localhost:5000/api?message=tensor takes you to the tensorlow solution
+#http://localhost:5000/api?message=openai takes you to the openai solution
 
 @app.before_request
 def check_url_and_redirect():
     if request.path[1:] == 'api':
-        if request.args.get('message') == "joo":
-            return redirect(url_for('joo'))
-        elif request.args.get('message') == "ei":
-            return redirect(url_for('ei'))
-        elif request.args.get('message') == "vaihda":
-            return redirect(url_for('vaihda'))
+        if request.args.get('message') == "tensor":
+            return redirect(url_for('tensorflow'))
+        elif request.args.get('message') == "openai":
+            return redirect(url_for('openai'))
+#if the message has over 20 marks, it goes to openai
+        if(len(request.args.get('message'))<20):
+            return redirect(url_for('tensorflow'))
         else:
-            return redirect(url_for('confused'))
-            
+            return redirect(url_for('openai'))
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
-    
-@app.route("/joo")
-def joo():
-    return "aika varattu"
-    
-@app.route("/ei")
-def ei():
-    return "ei sitte"
+    return "please input a message!"
 
-@app.route("/vaihda")
-def vaihda():
-    return "Mikäs aika sopis?"
+@app.route("/tensorflow")
+def tensorflow():
+    return "tensori"
 
-@app.route("/confused")
-def confused():
-    return "Nyt en ymmärtäny"
-    
+@app.route("/openai")
+def openai():
+    return "ouppeni"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
