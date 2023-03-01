5fe8e64f596ed4375ee1f4af80c8ec52a46e23a7
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-03-01 10:43:53 +0200

    A message is a part of the request now. Can receive text messages and JSON-messages.

--- backend/backend.py
+++ backend/backend.py
@@ -1,29 +1,42 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Use /classify-1/[your message], /classify-2/[your message] or /classify-3/[your message] to get an answer from the prototype "
+    return "Use /classify-1, /classify-2 or /classify-3 to get an answer from the prototype"
 
-@app.route("/classify-1/<msg>", methods=['GET', 'POST'])
-def tensorflow(msg):
-    #result = lassis_genius_bot(message)
-    return "from api 1 (tensorflow) with " + msg
+@app.route("/classify-1", methods=['GET', 'POST'])
+def tensorflow():
+    msg = request.form.get('message') #This receives texts
+    #msg = request.get_json()['message'] #This receives JSON format
+    if msg is not None:
+        #result = lassis_genius_bot(message)
+        return "from API 1 (tensorflow) with a message: " + msg 
+    else:
+        return "No message provided"
 
-@app.route("/classify-2/<msg>", methods=['GET', 'POST'])
-def combo(msg):
-    if(len(msg)<20):
-        #result = lassis_genius_bot(msg)
-        return "from api 2 (tensorflow) with " + msg
-    
-    #result = ermyas_genius_bot(msg)
-    return "from api 2 (open ai) with " + msg
+@app.route("/classify-2", methods=['GET', 'POST'])
+def combo():
+    msg = request.form.get('message') #This receives texts
+    #msg = request.get_json()['message'] #This receives JSON format
+    if msg is not None:
+        if (len(msg) < 20):
+            #result = lassis_genius_bot(msg)
+            return "from API 2 (tensorflow) with a message: " + msg
+        #result = ermyas_genius_bot(msg)
+        return "from API 2 (openAI) with a message: " + msg
+    else:
+        return "No message provided"
 
-@app.route("/classify-3/<msg>", methods=['GET', 'POST'])
-def openai(msg):
-    #result = ermyas_genius_bot(msg)
-    return "from api 3 (openai) with " + msg
-
+@app.route("/classify-3", methods=['GET', 'POST'])
+def openai():
+    msg = request.form.get('message') #This receives texts
+    #msg = request.get_json()['message'] #This receives JSON format
+    if msg is not None:
+        #result = ermyas_genius_bot(msg)
+        return "from API 3 (openAI) with a message: " + msg 
+    else:
+        return "No message provided"
 
 if __name__ == "__main__":
     app.run(debug=True)
