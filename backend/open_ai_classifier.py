94b50704dde737212ca22d8b82ce2f9aa8d5acff
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2023-04-06 12:56:39 +0300

    Update open_ai_classifier.py

--- backend/open_ai_classifier.py
+++ backend/open_ai_classifier.py
@@ -1,6 +1,6 @@
 import openai
 import re
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def open_ai_classifier(response_text):
